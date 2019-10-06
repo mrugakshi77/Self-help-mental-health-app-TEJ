3626d05b63c54cc62c2ae7d5ce0a62f671754613
mrugakshi77 <mrugakshi@yahoo.com>
2019-10-06 14:46:28 +0530

    TextInputLayout error message issue solved

---
+++ app/src/main/java/com/example/mentalhealth/CustomTextInputLayout.java
@@ -0,0 +1,45 @@
+package com.example.mentalhealth;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import androidx.annotation.Nullable;
+
+public class CustomTextInputLayout extends TextInputLayout {
+    public CustomTextInputLayout(Context context) {
+        super(context);
+    }
+
+    public CustomTextInputLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomTextInputLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void drawableStateChanged() {
+        super.drawableStateChanged();
+        clearEditTextColorfilter();
+    }
+    @Override
+    public void setError(@Nullable CharSequence error) {
+        super.setError(error);
+        clearEditTextColorfilter();
+    }
+
+    private void clearEditTextColorfilter() {
+        EditText editText = getEditText();
+        if (editText != null) {
+            Drawable background = editText.getBackground();
+            if (background != null) {
+                background.clearColorFilter();
+            }
+        }
+    }
+}
