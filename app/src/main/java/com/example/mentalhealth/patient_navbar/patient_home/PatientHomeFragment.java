34320c058c0385bccfc5f2a82e730a011dd51d7a
mrugakshi77 <mrugakshi@yahoo.com>
2019-10-07 10:49:42 +0530

    Posts' UI commit pt.2

--- app/src/main/java/com/example/mentalhealth/patient_navbar/patient_home/PatientHomeFragment.java
+++ app/src/main/java/com/example/mentalhealth/patient_navbar/patient_home/PatientHomeFragment.java
@@ -8,6 +8,8 @@ import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
