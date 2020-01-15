483567ee16e12983785e69dbf05b04fd1f845d2a
apoorvakhuspe <apoorvakhuspe@gmail.com>
2020-01-15 11:59:34 +0530

    Questionnaire part3

--- app/src/main/java/com/example/mentalhealth/Questionnaire.java
+++ app/src/main/java/com/example/mentalhealth/Questionnaire.java
@@ -5,12 +5,31 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.RadioGroup;
-import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.common.FirebaseMLException;
+import com.google.firebase.ml.common.modeldownload.FirebaseModelDownloadConditions;
+import com.google.firebase.ml.common.modeldownload.FirebaseModelManager;
+import com.google.firebase.ml.custom.FirebaseCustomRemoteModel;
+import com.google.firebase.ml.custom.FirebaseModelDataType;
+import com.google.firebase.ml.custom.FirebaseModelInputOutputOptions;
+import com.google.firebase.ml.custom.FirebaseModelInputs;
+import com.google.firebase.ml.custom.FirebaseModelInterpreter;
+import com.google.firebase.ml.custom.FirebaseModelInterpreterOptions;
+import com.google.firebase.ml.custom.FirebaseModelOutputs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Questionnaire extends AppCompatActivity {
 
@@ -27,6 +46,57 @@ public class Questionnaire extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questionnaire);
+
+        final FirebaseCustomRemoteModel remoteModel =
+                new FirebaseCustomRemoteModel.Builder("Questionnaire").build();
+
+        FirebaseModelDownloadConditions conditions = new FirebaseModelDownloadConditions.Builder()
+                .requireWifi()
+                .build();
+        FirebaseModelManager.getInstance().download(remoteModel, conditions)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // Success.
+                    }
+                });
+
+        FirebaseModelManager.getInstance().isModelDownloaded(remoteModel)
+                .addOnSuccessListener(new OnSuccessListener<Boolean>() {
+                    @Override
+                    public void onSuccess(Boolean isDownloaded) {
+                        FirebaseModelInterpreterOptions options;
+                        //if (isDownloaded) {
+                            options = new FirebaseModelInterpreterOptions.Builder(remoteModel).build();
+                        //}
+                        try {
+                            FirebaseModelInterpreter interpreter = FirebaseModelInterpreter.getInstance(options);
+                        } catch (FirebaseMLException e) {
+                            e.printStackTrace();
+                        }
+                        // ...
+                    }
+                });
+
+        FirebaseModelManager.getInstance().download(remoteModel, conditions)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void v) {
+                        // Download complete. Depending on your app, you could enable
+                        // the ML feature, or switch from the local model to the remote
+                        // model, etc.
+                    }
+                });
+
+        final FirebaseModelInputOutputOptions inputOutputOptions;
+        try {
+            inputOutputOptions = new FirebaseModelInputOutputOptions.Builder()
+                    .setInputFormat(0, FirebaseModelDataType.FLOAT32, new int[]{1,20})
+                    .setOutputFormat(0, FirebaseModelDataType.FLOAT32, new int[]{1, 4})
+                    .build();
+        } catch (FirebaseMLException e) {
+            e.printStackTrace();
+        }
 
         questionnaireViewModel = ViewModelProviders.of(this).get(QuestionnaireViewModel.class);
 
@@ -55,6 +125,60 @@ public class Questionnaire extends AppCompatActivity {
                 Log.d("VALUES:", str3);
                 Log.d("VALUES:", str4);
 
+
+                FirebaseModelInputs inputs = null;
+                try {
+                    inputs = new FirebaseModelInputs.Builder()
+                            .add(selected_values)  // add() as many input arrays as your model requires
+                            .build();
+                } catch (FirebaseMLException e) {
+                    e.printStackTrace();
+                }
+                FirebaseModelInterpreter firebaseInterpreter = null;
+                try {
+                    firebaseInterpreter = createInterpreter(remoteModel);
+                } catch (FirebaseMLException e) {
+                    e.printStackTrace();
+                }
+
+                firebaseInterpreter.run(inputs, inputOutputOptions)
+                        .addOnSuccessListener(
+                                new OnSuccessListener<FirebaseModelOutputs>() {
+                                    @Override
+                                    public void onSuccess(FirebaseModelOutputs result) {
+                                        // ...
+                                        float[][] output = result.getOutput(0);
+                                        float[] probabilities = output[0];
+
+                                        BufferedReader reader = null;
+                                        try {
+                                            reader = new BufferedReader(
+                                                    new InputStreamReader(getAssets().open("retrained_labels.txt")));
+                                        } catch (IOException e) {
+                                            e.printStackTrace();
+                                        }
+                                        for (int i = 0; i < probabilities.length; i++) {
+                                            String label = null;
+                                            try {
+                                                label = reader.readLine();
+                                            } catch (IOException e) {
+                                                e.printStackTrace();
+                                            }
+                                            Log.i("MLKit", String.format("%s: %1.4f", label, probabilities[i]));
+                                        }
+
+                                    }
+                                })
+                        .addOnFailureListener(
+                                new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        // Task failed with an exception
+                                        // ...
+                                    }
+                                });
+
+
                 //Toast.makeText(getApplicationContext(), selected_values[1], Toast.LENGTH_LONG).show();
 
                 Intent i = new Intent(Questionnaire.this, Patient_feed.class);
@@ -64,5 +188,19 @@ public class Questionnaire extends AppCompatActivity {
 
     }
 
+    private FirebaseModelInterpreter createInterpreter(FirebaseCustomRemoteModel remoteModel) throws FirebaseMLException {
+        // [START mlkit_create_interpreter]
+        FirebaseModelInterpreter interpreter = null;
+        try {
+            FirebaseModelInterpreterOptions options =
+                    new FirebaseModelInterpreterOptions.Builder(remoteModel).build();
+            interpreter = FirebaseModelInterpreter.getInstance(options);
+        } catch (FirebaseMLException e) {
+            // ...
+        }
+        // [END mlkit_create_interpreter]
+
+        return interpreter;
+    }
 
 }
