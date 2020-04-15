5ee3ccd0f4e3bc188dc2b03dc531988919c5731c
mrugakshi77 <mrugakshi@yahoo.com>
2020-04-15 12:05:32 +0530

    upload dp_patient part 1

--- app/src/main/java/com/example/mentalhealth/PatientInfoActivity.java
+++ app/src/main/java/com/example/mentalhealth/PatientInfoActivity.java
@@ -7,15 +7,27 @@ import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.MimeTypeMap;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.mentalhealth.databinding.ActivityPatientInfoBinding;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -23,6 +35,14 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import static androidx.constraintlayout.widget.Constraints.TAG;
 
 
 public class PatientInfoActivity extends AppCompatActivity {
@@ -34,11 +54,25 @@ public class PatientInfoActivity extends AppCompatActivity {
     Spinner marital_status;
     Spinner profession;
     EditText describe_patient;
+    private DatabaseReference databaseRef, dbref;
+    private StorageReference storageReference;
+    private static int PICK_IMAGE_REQUEST = 1;
+    private Uri uri;
+    private StorageTask uploadTask;
+    private ImageView displayPicture_patient;
+
+    private String uploadKey;
+    private String uname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_info);
+
+        storageReference = FirebaseStorage.getInstance().getReference("posts");
+        dbref = FirebaseDatabase.getInstance().getReference("User");
+        //databaseReference = FirebaseDatabase.getInstance().getReference("U");
+        databaseRef = FirebaseDatabase.getInstance().getReference("downloadableURLs");
 
         ActivityPatientInfoBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_patient_info);
         final PatientInfoViewModel patientInfoViewModel = ViewModelProviders.of(this).get(PatientInfoViewModel.class);
@@ -48,10 +82,18 @@ public class PatientInfoActivity extends AppCompatActivity {
         this.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         this.getWindow().setBackgroundDrawableResource(R.color.fbutton_color_transparent);
 
+        displayPicture_patient = findViewById(R.id.displayPicture_patient);
         fButton_setDp =findViewById(R.id.setDp_patient);
         fButton_submit =findViewById(R.id.submit_patient);
         fButton_setDp.setButtonColor(getResources().getColor(R.color.colorWhite));
         fButton_submit.setButtonColor(getResources().getColor(R.color.colorWhite));
+
+        fButton_setDp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFileChooser();
+            }
+        });
 
         marital_status = (Spinner) findViewById(R.id.marital_status);
         ArrayAdapter<String> myAdapter1 = new ArrayAdapter<String>(PatientInfoActivity.this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.marital_status));
@@ -74,6 +116,7 @@ public class PatientInfoActivity extends AppCompatActivity {
                 //}
             }
         });
+
     }
 
     private void savePatientInformation(final PatientInfo patientInfo) {
@@ -91,6 +134,9 @@ public class PatientInfoActivity extends AppCompatActivity {
                     databaseReference.child("User").child(curr_user.getEmail().replace('.','&')).child("Profession").setValue(profession.getSelectedItem().toString());
                     databaseReference.child("User").child(curr_user.getEmail().replace('.','&')).child("Description").setValue(patientInfo.getDescribe());
 
+                    //databaseReference.child("User").child(curr_user.getEmail().replace('.','&')).child("Profile Photo").setValue(patientInfo.getDescribe());
+
+                    uploadFile();
                     //Toast.makeText(getApplicationContext(), "Saving Patient Info", Toast.LENGTH_LONG).show();
 
                     redirect(curr_user);
@@ -108,4 +154,102 @@ public class PatientInfoActivity extends AppCompatActivity {
         Intent i = new Intent(PatientInfoActivity.this, DepressionTest.class);
         startActivity(i);
     }
+
+
+    private void openFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    private String getFileExtension(Uri uri) {
+        ContentResolver contentResolver = getApplicationContext().getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            uri = data.getData();
+            Picasso.get().load(uri).into(displayPicture_patient);
+            displayPicture_patient.setImageURI(uri);
+        }
+    }
+
+
+    private void uploadFile() {
+        if (uri != null) {
+            final StorageReference reference = storageReference.child(System.currentTimeMillis() + "." + getFileExtension(uri));
+            uploadTask = reference.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Handler handler = new Handler();
+                    /*handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            //progressBar.setProgress(0);
+                        }
+                    }, 5000);*/
+                    Toast.makeText(getApplicationContext(), "Upload Successful", Toast.LENGTH_LONG).show();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                    double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                    //progressBar.setProgress((int) progress);
+                }
+            });
+            reference.putFile(uri).continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                @Override
+                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                    if (!task.isSuccessful()) {
+                        throw task.getException();
+                    }
+                    return reference.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    if (task.isSuccessful()) {
+                        Uri downloadUri = task.getResult();
+                        Log.d(TAG, "onComplete: Url: " + downloadUri.toString());
+                        //final com.example.mentalhealth.Model.Upload upload = new com.example.mentalhealth.Model.Upload(editText.getText().toString().trim(), downloadUri.toString());
+
+                        dbref.addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                FirebaseUser curr_user = FirebaseAuth.getInstance().getCurrentUser();
+                                //uploadPhoto(dataSnapshot.child(curr_user.getEmail().replace('.', '&')).child("mName").getValue().toString());
+
+                                //uploadKey = databaseReference.push().getKey();
+                                dbref.child("User").child(curr_user.getEmail().replace('.', '&')).child("dp").setValue(uri);
+                            }
+
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                            }
+                        });
+                        //upload.setUserName(uname);
+                        // uploadKey = databaseReference.push().getKey();
+                        //databaseReference.child(uploadKey).setValue(upload);
+
+                    }
+                }
+            });
+
+        } else
+            Toast.makeText(getApplicationContext(), "No file selected", Toast.LENGTH_SHORT).show();
+    }
+
 }
