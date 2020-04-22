621c7a41502f69c124c163f8e24f601f9777c2ab
apoorvakhuspe <apoorvakhuspe@gmail.com>
2020-04-23 02:00:58 +0530

    UI part3

--- app/src/main/java/com/example/mentalhealth/LoginActivity.java
+++ app/src/main/java/com/example/mentalhealth/LoginActivity.java
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
